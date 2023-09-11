f24dbd46e81f4ae6d6ef7a2bb4b67ed2ed7e9aea
janmejoy2 <133534345+janmejoy2@users.noreply.github.com>
2023-09-12 02:07:29 +0530

    Add files via upload

---
+++ src/main/java/com/sample/api/SpringApiApplication.java
@@ -0,0 +1,27 @@
+package com.sample.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringApiApplication {
+
+	@GetMapping("/")
+	public String getMessage() {
+		return "Sample Spring Boot Api is Running on Red Hat OpenShift";
+	}
+	
+	@PostMapping("/{name}")
+	public String postMessage(@PathVariable String name) {
+		return "The Api is build by: " + name;
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiApplication.class, args);
+	}
+
+}
